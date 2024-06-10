7482927fda61dad921dad25de9f7529ce7237b6a
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-06-10 15:18:00 +0500

    added sql script

--- src/main/resources/fix_mod_consortia_table.sql
+++ src/main/resources/fix_mod_consortia_table.sql
@@ -1,20 +1,20 @@
 DO $$
-DECLARE
-    target_tenant TEXT:= 'consortium';
-    source_tenant TEXT:= 'college';
-    record RECORD;
 BEGIN
-    -- Delete existing records in <source_tenant>_mod_consortia where target_tenant_id matches the target tenant
-    DELETE FROM college_mod_consortia.sharing_instance
-    WHERE target_tenant_id = target_tenant;
+    -- There are two variable need to change to appropriate tenant names:
+    -- 1. central_tenant - source tenant
+    -- 2. member_tenant - target tenant
+    -- e.g: central_tenant to consortium, member_tenant to college
+    -- Delete existing records in <central_tenant>_mod_consortia where target_tenant_id matches the target tenant
+    DELETE FROM <central_tenant>_mod_consortia.sharing_instance
+    WHERE target_tenant_id = '<member_tenant>';
 
-    -- Select records from the  <target_tenant>_mod_inventory_storage.instance table where the "source" is 'CONSORTIUM-MARC'
+    -- Select records from the <member_tenant>_mod_inventory_storage.instance table where the "source" is 'CONSORTIUM-MARC'
     WITH record_from_target_tenant as (
-        SELECT id FROM consortium_mod_inventory_storage.instance
+        SELECT id FROM <member_tenant>_mod_inventory_storage.instance
         WHERE jsonb->>'source' = 'CONSORTIUM-MARC'
     )
-    -- Insert each record into <source_tenant>_mod_consortia
-    INSERT INTO college_mod_consortia.sharing_instance (id, instance_id, source_tenant_id, target_tenant_id, status)
-    SELECT rs.id, rs.id, source_tenant, target_tenant, 'COMPLETED'
+    -- Insert each record into <central_tenant>_mod_consortia
+    INSERT INTO <central_tenant>_mod_consortia.sharing_instance (id, instance_id, source_tenant_id, target_tenant_id, status)
+    SELECT rs.id, rs.id, '<central_tenant>', '<member_tenant>', 'COMPLETED'
     FROM record_from_target_tenant as rs;
 END $$;
